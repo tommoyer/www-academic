55a38432b85b5a9cdb9532f76ffe733aca8b82b7
Thomas Moyer <thomas.moyer@ll.mit.edu>
2012-07-13 06:37:40 -0400

    Initial commit

---
+++ wiki/tiddlywebconfig.py
@@ -0,0 +1,14 @@
+# A basic configuration.
+# Run "pydoc tiddlyweb.config" for details on configuration items.
+
+config = {
+    'system_plugins': ['tiddlywebwiki'],
+    'secret': '1e9350bc6009737a73a403de2421267c6240a622',
+    'twanager_plugins': ['tiddlywebwiki'],
+    'server_prefix': '/wiki',
+    'server_host': {
+        'scheme': 'https',
+        'host': 'thomasmoyer.org',
+        'port': '443',
+    },
+}
