b98024558765cff1e69e3cc0b543e2044f41c05b
Thomas Moyer <thomas.moyer@ll.mit.edu>
2014-06-02 14:32:45 -0400

    Cleanup and restart

---
+++ publish.el
@@ -0,0 +1,50 @@
+#!/usr/local/bin/emacs --script
+;; 
+;; Usage:
+;;    publish.el
+
+
+;; Mark org-related variables as safe local variables,
+;; regardless of their value.
+(defun my/always-safe-local-variable (val) t)
+(dolist (sym '(org-export-publishing-directory
+               org-export-html-preamble
+               org-export-html-postamble))
+  (put sym 'safe-local-variable 'my/always-safe-local-variable))
+
+(load "~/.emacs.d/init.el")
+
+(setq org-publish-project-alist '(
+	("site-content"
+	 :base-directory "~/Repos/www"
+	 :base-extension "org"
+	 :publishing-directory "~/Public/"
+	 :recursive t
+	 :publishing-function org-html-publish-to-html
+	 :exclude "level-0.org"
+	 )
+
+	("site-static"
+         :base-directory "~/Repos/www"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|ico"
+         :publishing-directory "~/Public/"
+         :recursive t
+         :publishing-function org-publish-attachment)
+
+	("site" :components ("site-content" "site-static"))))
+
+(defun tm/site-preamble (options)
+  "The function that creates the preamble (sidebar) for the blog.
+OPTIONS contains the property list from the org-mode export."
+  (let ((base-directory (plist-get options :base-directory)))
+    (org-babel-with-temp-filebuffer (expand-file-name "html/preamble.html" base-directory) (buffer-string))))
+
+(defun tm/site-postamble (options)
+  "The function that creates the preamble (sidebar) for the blog.
+OPTIONS contains the property list from the org-mode export."
+  (let ((base-directory (plist-get options :base-directory)))
+    (org-babel-with-temp-filebuffer (expand-file-name "html/postamble.html" base-directory) (buffer-string))))
+
+(shell-command "find ~/Repos/www -name \"*.org\" -exec touch '{}' \;")
+
+(org-publish-project "site" t)
