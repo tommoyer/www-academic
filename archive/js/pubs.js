f381a53c5db003ca42535b3591e0a24a7dffb824
Thomas Moyer <thomas.moyer@ll.mit.edu>
2016-06-16 23:48:03 -0400

    First attempt at a new Jekyll-based site with a decent looking theme

--- archive/js/pubs.js
+++
@@ -1,9 +0,0 @@
-function bibtex_js_draw() {
-
-$(".bibtex_template").hide();
-(new BibtexDisplay()).displayBibtex($("#bibtex_input").val(), $("#bibtex_display"));
-}
-$(document).ready( function() {
-
-$("#bibtex_input").load( "files/pubs.bib", bibtex_js_draw );
-} );
